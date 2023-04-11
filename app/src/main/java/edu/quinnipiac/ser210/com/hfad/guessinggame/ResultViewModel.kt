edf34ccdd453ed7c060b397359e20dc80f676956
cakeller2 <cakeller2@quinnipiac.edu>
2023-04-11 01:11:16 -0400

    Finished Guessing Game

---
+++ app/src/main/java/edu/quinnipiac/ser210/com/hfad/guessinggame/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package edu.quinnipiac.ser210.com.hfad.guessinggame
+
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel(finalResult: String): ViewModel() {
+    val result = finalResult
+}
